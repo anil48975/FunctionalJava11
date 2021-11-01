7ad51c53fdad2ddbc8dbe8284b6d2d188a10ce0e
anil48975 <anil.tiwari.2007.5499@gmail.com>
2021-11-01 10:03:10 +0530

    Delete ItemDBRepositoryImpl.java

--- systemdesign/repository/ItemDBRepositoryImpl.java
+++
@@ -1,9 +0,0 @@
-package systemdesign.repository;
-
-import systemdesign.repository.entity.ItemEntity;
-
-public class ItemDBRepositoryImpl implements ItemDBRepository{
-    public void save(ItemEntity entity) {
-        System.out.println("Reading from Queue, item: " + entity.toString());
-    }
-}
